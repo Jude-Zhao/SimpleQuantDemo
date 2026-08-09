9b1852230ce2278e1c87f1e71a45059d0b180a49
jude <jude_z@mail.ustc.edu.cn>
2026-08-09 13:32:25 +0800

    refactor(synthesis): FAA/EAA 纯函数下沉 core，webapp 改 re-export，新增 bt 依赖

--- core/synthesis/__init__.py
+++ core/synthesis/__init__.py
@@ -1,7 +1,27 @@
 """Factor synthesis utilities."""
 
 from core.synthesis.base import FactorSynthesizer
+from core.synthesis.faa_eaa import (
+    EPS,
+    Resolver,
+    build_category_factors,
+    build_category_scores,
+    eaa_composite,
+    faa_composite,
+    normalize_cross_section,
+)
 from core.synthesis.icir_weight import ICIRWeightedSynthesizer, calculate_decayed_icir_score
 
-__all__ = ["FactorSynthesizer", "ICIRWeightedSynthesizer", "calculate_decayed_icir_score"]
+__all__ = [
+    "EPS",
+    "FactorSynthesizer",
+    "ICIRWeightedSynthesizer",
+    "Resolver",
+    "build_category_factors",
+    "build_category_scores",
+    "calculate_decayed_icir_score",
+    "eaa_composite",
+    "faa_composite",
+    "normalize_cross_section",
+]
 
